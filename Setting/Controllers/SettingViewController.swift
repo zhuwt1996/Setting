d08bdec27568b86e16055c0ab7438e2a10b59cc2
zwt <1255284280@qq.com>
2019-10-10 11:21:52 +0800

    初始化

---
+++ Setting/Controllers/SettingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SettingViewController.swift
+//  Setting
+//
+//  Created by ZWT on 2019/10/10.
+//  Copyright © 2019 ZWT. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "设置"
+        self.view.backgroundColor = UIColor.white
+        
+        setupUI()
+    }
+    
+    fileprivate func setupUI(){
+        
+    }
+
+}
